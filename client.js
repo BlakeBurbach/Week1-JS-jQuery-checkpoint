bc9e1bade4183e4667fe91326dc20c35bcb3ac7e
Blake Burbach <cras.bburbach@gmail.com>
2018-03-12 13:32:57 -0500

    created a p tag inside of new div element that displays how many times the button has been clicked on the DOM.

--- client.js
+++ client.js
@@ -2,6 +2,10 @@
 // when document is loaded, run readyNow
 $(document).ready(readyNow);
 
+// global variable for how many times button has been clicked
+// able to use in any function
+let numberOfButtonClicks = 0;
+
 // readyNow function activated when document is ready
 function readyNow() {
   // when Generate button is clicked - create a div element
@@ -10,5 +14,11 @@ function readyNow() {
 
 // function that creates a div element when generate button is clicked
 function addDiv() {
-  $('#newDivs').append( '<div></div>' );
+  $('#newDivs').empty();
+  let newDivElement = $('<div id="newDivElement"></div>');
+  $('#newDivs').append( newDivElement );
+  numberOfButtonClicks ++;
+  console.log( numberOfButtonClicks );
+  let numberParElement = $( '<p id="newParElement">Number of Button Clicks: '+numberOfButtonClicks+'</p>' );
+  newDivElement.append(numberParElement);
 } // end addDiv
