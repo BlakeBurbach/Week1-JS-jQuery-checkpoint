95f9263815cfaaba717508387af7fbc215632e21
Blake Burbach <cras.bburbach@gmail.com>
2018-03-12 13:20:24 -0500

    button now creates new div elements on DOM when clicked.

--- client.js
+++ client.js
@@ -1,7 +1,14 @@
-console.log('js');
 
+// when document is loaded, run readyNow
 $(document).ready(readyNow);
 
+// readyNow function activated when document is ready
 function readyNow() {
-  console.log( 'jQuery' );
-}
+  // when Generate button is clicked - create a div element
+  $( '#generateBtn' ).on( 'click', addDiv );
+} // end readyNow
+
+// function that creates a div element when generate button is clicked
+function addDiv() {
+  $('#newDivs').append( '<div></div>' );
+} // end addDiv
