3ffb7033b615c46ca483b378fbcb366eea7f47bf
Blake Burbach <cras.bburbach@gmail.com>
2018-03-12 13:35:43 -0500

    added code comments to js.

--- client.js
+++ client.js
@@ -14,11 +14,18 @@ function readyNow() {
 
 // function that creates a div element when generate button is clicked
 function addDiv() {
+  // empty newDivs every time button is clicked so there's not a huge list of new
+  // div elements. just looks like one.
   $('#newDivs').empty();
+  // variable to hold each newly created div element
   let newDivElement = $('<div id="newDivElement"></div>');
+  // append each newly created div to the dom
   $('#newDivs').append( newDivElement );
+  // increase number of times button has been clicked
   numberOfButtonClicks ++;
   console.log( numberOfButtonClicks );
+  // variable holding a newly created p tag inside of new div element
+  // that will display the number of times button has been clicked
   let numberParElement = $( '<p id="newParElement">Number of Button Clicks: '+numberOfButtonClicks+'</p>' );
   newDivElement.append(numberParElement);
 } // end addDiv
